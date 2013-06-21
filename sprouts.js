d940b54f5d3bcabd18d9a5982f4d00892b66bab6
François Pinard <pinard@iro.umontreal.ca>
2013-06-20 23:10:29 -0400

    sprout.js: Use layers to group curves, and to group sprouts.

--- sprouts.js
+++ sprouts.js
@@ -1,10 +1,19 @@
 // Sprouts game board.
 // François Pinard, 2013-06.
 
-var curves = [];
-var sprouts = [];
+// Layers.
+var curve_layer = project.activeLayer;
+var sprout_layer = new Layer();
 
+// Curves.
+tool.minDistance = 15;
+var path = null;
+
+// Sprouts.
+var starting_sprout;
 add_initial_sprouts(3);
+
+// Functions.
 
 function add_initial_sprouts(count) {
   var delta = 360 / count;
@@ -13,9 +22,9 @@ function add_initial_sprouts(count) {
   offset.length = Math.min(size.width, size.height) * 1 / 3;
   offset.angle = Math.random() * delta;
   for (var counter = 0; counter < count; counter++) {
-    sprouts.push(new_sprout(view.center + offset));
+    new_sprout(view.center + offset);
     offset.angle += delta;
-  };
+  }
 }
 
 function add_links(sprout1, sprout2) {
@@ -31,7 +40,23 @@ function add_links(sprout1, sprout2) {
   }
 }
 
+function nearest_sprout(point) {
+  var best = null;
+  var counter;
+  for (counter = 0; counter < sprout_layer.children.length; counter++) {
+    var sprout = sprout_layer.children[counter];
+    var distance = (sprout.data.center - point).length
+    if (distance < 100 && sprout.data.links.length < 3) {
+      if (best == null || 2 * distance < (best.data.center - point).length) {
+        best = sprout;
+      }
+    }
+  };
+  return best;
+}
+
 function new_sprout(center) {
+  sprout_layer.activate()
   var sprout = new Path.Circle({
     center: center,
     radius: 12,
@@ -54,82 +79,66 @@ function new_sprout(center) {
   return sprout;
 }
 
-function nearest_sprout(point) {
-  var best = null;
-  for (var counter = 0; counter < sprouts.length; counter++) {
-    var sprout = sprouts[counter];
-    var distance = (sprout.data.center - point).length
-    if (distance < 100 && sprout.data.links.length < 3) {
-      if (best == null) {
-        best = sprout;
-      } else if (2 * distance < (best.data.center - point).length) {
-        best = sprout;
-      }
-    }
-  };
-  return best;
-}
-
-tool.minDistance = 15;
-var path;
-var starting_sprout;
-
 function onMouseDown(event) {
   starting_sprout = nearest_sprout(event.point);
-  path = null;
-  if (starting_sprout != null) {
-    var point = starting_sprout.getNearestLocation(event.point);
-    path = new Path({
-      segments: [point],
-      strokeColor: 'brown',
-      strokeWidth: 3
-    })
-  }
+  if (starting_sprout == null) return;
+  var point = starting_sprout.getNearestLocation(event.point);
+  curve_layer.activate();
+  path = new Path({
+    segments: [point],
+    strokeColor: 'brown',
+    strokeWidth: 3
+  })
 }
 
 function onMouseDrag(event) {
-  if (path != null) {
-    path.add(event.point);
-    for (counter = 0; counter < curves.length; counter++) {
-      if (path.getIntersections(curves[counter]).length > 0) {
-        path.remove();
-        path = null;
-        return;
-      }
-    };
-    // for (counter = 0; counter < sprouts.length; counter++) {
-    //   if (path.getIntersections(sprouts[counter]).length > 0) {
-    //     path.remove();
-    //     path = null;
-    //     return;
-    //   }
-    // }
+  if (path == null) return;
+  path.add(event.point);
+  if (path_touches_anything()) {
+    path.remove();
+    path = null;
   }
 }
 
 function onMouseUp(event) {
-  if (path != null) {
-    if (path._segments.length < 3) {
-      // Prevent sprouts overlapping.
-      path.remove();
-    } else {
-      var ending_sprout = nearest_sprout(event.point);
-      if (ending_sprout == null) {
-        path.remove();
-      } else if (ending_sprout == starting_sprout &&
-                 starting_sprout.data.links.length > 1) {
-        // Prevent looping on a sprout with no room for two more links.
-        path.remove()
-      } else {
-        var point = ending_sprout.getNearestLocation(event.point);
-        path.add(point);
-        path.simplify(10);
-        curves.push(path);
-        var sprout = new_sprout(path.getPointAt(path.length / 2));
-        sprouts.push(sprout);
-        add_links(sprout, starting_sprout);
-        add_links(sprout, ending_sprout);
-      }
-    }
+  if (path == null) return;
+  if (path._segments.length < 3) {
+    // Prevent sprouts overlapping.
+    path.remove();
+    path = null;
+    return;
   }
+  var ending_sprout = nearest_sprout(event.point);
+  if (ending_sprout == null) {
+    path.remove();
+    path = null;
+    return;
+  }
+  if (ending_sprout == starting_sprout &&
+      starting_sprout.data.links.length > 1) {
+    // Prevent looping on a sprout with no room for two more links.
+    path.remove();
+    path = null;
+    return;
+  }
+  var point = ending_sprout.getNearestLocation(event.point);
+  path.add(point);
+  if (path_touches_anything()) {
+    path.remove();
+    path = null;
+    return;
+  }
+  path.simplify(10);
+  var sprout = new_sprout(path.getPointAt(path.length / 2));
+  path = null;
+  add_links(sprout, starting_sprout);
+  add_links(sprout, ending_sprout);
+}
+
+function path_touches_anything() {
+  for (var counter = 0; counter < curve_layer.children.length; counter++) {
+    curve = curve_layer.children[counter]
+    if (curve != path && path.getIntersections(curve).length > 0) return true;
+  };
+  return false;
 }
