e48530d8b06d30ed42dadae98ebbabf25630e5cb
François Pinard <pinard@iro.umontreal.ca>
2013-06-22 12:04:35 -0400

    sprouts.js: Experiment on dynamic moving of sprouts.

--- sprouts.js
+++ sprouts.js
@@ -1,6 +1,9 @@
 // Sprouts game board.
 // François Pinard, 2013-06.
 
+function log(text) { console.log(text); }
+function log(text) { }
+
 // Layers.
 var curve_layer = project.activeLayer;
 var sprout_layer = new Layer();
@@ -12,7 +15,8 @@ var drawing = null;
 // Sprouts.
 var radius = 12;
 var starting_sprout;
-add_initial_sprouts(3);
+add_initial_sprouts(50);
+var equilibrium = 200;
 
 // Functions.
 
@@ -20,8 +24,9 @@ function add_initial_sprouts(count) {
   var delta = 360 / count;
   var offset = new Point();
   var size = view.size;
-  offset.length = Math.min(size.width, size.height) * 1 / 3;
-  offset.angle = Math.random() * delta;
+  offset.length = Math.min(size.width, size.height) * 1 / 10;
+  offset.angle = 0;
+  //offset.angle = Math.random() * delta;
   for (var counter = 0; counter < count; counter++) {
     new_sprout(view.center + offset);
     offset.angle += delta;
@@ -86,26 +91,94 @@ function new_sprout(center) {
   return sprout;
 }
 
+var slowness = 1;
+var frame_count = 0;
+
 function onFrame(event) {
   if (drawing != null) return;
-  var barysum = new Point(0, 0);
-  sprout_layer.children.forEach(function(sprout) {
-    barysum += sprout.data.center;
-  });
-  var delta = view.center - (barysum / sprout_layer.children.length);
-  if (Math.abs(delta.x) > Math.abs(delta.y) + 2) {
-    delta = (delta / Math.abs(delta.x)).round();
-  } else if (Math.abs(delta.y) > Math.abs(delta.x) + 2) {
-    delta = (delta / Math.abs(delta.y)).round();
-  } else {
+
+  if (frame_count == 1000) {
+    drawing = new Path();
     return;
-  }
-  project.layers.forEach(function(layer) {
-    layer.children.forEach(function(path) {
-      translate_path(path, delta);
-      if (layer == sprout_layer) path.data.center += delta;
-    })
-  })
+  };
+
+  frame_count += 1;
+  if (frame_count == 1000000) frame_count = 0;
+  if (frame_count % slowness != 0) return;
+
+  var sprouts = sprout_layer.children;
+
+  // Compute view extent and barycenter of sprouts.
+  var sum = new Point(0, 0);
+  var extent = new Rectangle(0, 0, 0, 0);
+  sprouts.forEach(function(sprout) {
+    var center = sprout.data.center;
+    sum += center;
+    if (center.x < extent.left) extent.left = center.x;
+    if (center.x > extent.right) extent.right = center.x;
+    if (center.y < extent.top) extent.top = center.y;
+    if (center.x > extent.bottom) extent.bottom = center.y;
+  });
+  var barycenter = sum / sprouts.length;
+
+  // Change equilibrium to shrink or grow, as needed.
+  if (frame_count % 30 == 0) {
+    if (extent.size.x > view.size.x || extent.size.y > view.size.y) {
+      equilibrium -= 1;
+    } else {
+      equilibrium += 1;
+    };
+    log('equilibrium = ' + equilibrium);
+  };
+
+  // Start with an overall translation.
+  var vector = view.center - barycenter;
+  sprouts.forEach(function(sprout) {
+    sprout.data.delta = vector.clone();
+    log('*over ' + sprout + ' center = ' + sprout.data.center
+       + ' delta = ' + sprout.data.delta);
+  });
+
+  // Add interaction goals between sprouts.
+  for (var counter2 = 1; counter2 < sprouts.length; counter2++) {
+    var sprout2 = sprouts[counter2];
+    for (var counter1 = 0; counter1 < counter2; counter1++) {
+      var sprout1 = sprouts[counter1];
+      vector = sprout2.data.center - sprout1.data.center;
+      log('*vec avant = ' + vector + ' equil = ' + equilibrium
+         + ' length = ' + vector.length);
+      vector.length -= equilibrium;
+      log('*vec après = ' + vector);
+      log('*s1 avant ' + sprout1 + ' delta = ' + sprout1.data.delta);
+      sprout1.data.delta += vector;
+      log('*s1 après ' + sprout1 + ' delta = ' + sprout1.data.delta);
+      log('*s2 avant ' + sprout2 + ' delta = ' + sprout2.data.delta);
+      sprout2.data.delta -= vector;
+      log('*s2 après ' + sprout2 + ' delta = ' + sprout2.data.delta);
+    }
+  };
+
+  // Move sprouts.
+  sprouts.forEach(function(sprout) {
+    log('*move ' + sprout + ' delta = ' + sprout.data.delta);
+    var delta = sprout.data.delta;
+    if (Math.abs(delta.x) > Math.abs(delta.y) + 2) {
+      delta /= Math.abs(delta.x);
+    } else if (Math.abs(delta.y) > Math.abs(delta.x) + 2) {
+      delta /= Math.abs(delta.y);
+    } else {
+      return;
+    };
+    translate_path(sprout, delta);
+    sprout.data.center += delta;
+  });
+
+  //project.layers.forEach(function(layer) {
+  //  layer.children.forEach(function(path) {
+  //    translate_path(path, delta);
+  //    if (layer == sprout_layer) path.data.center += delta;
+  //  })
+  //})
 }
 
 function onMouseDown(event) {
