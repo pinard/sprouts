027967141a378ca8d1a2ec05ea6a11cb2907ba29
François Pinard <pinard@iro.umontreal.ca>
2013-12-30 10:19:17 -0500

    sprout.js: Cleaned up with the help of jshint.

--- sprouts.js
+++ sprouts.js
@@ -1,10 +1,15 @@
 // Sprouts game board.
 // François Pinard, 2013-06.
 
+/* global angular, paper */
+/* exported log, Controller */
+/* jshint indent: 2, latedef: false */
+
 var debug = false;
 var initial_count = 3;
 
 function log(text) {
+  /* jshint devel: true */
   if (debug) console.log(text);
 }
 
@@ -17,12 +22,12 @@ function log(text) {
 
 function Controller($scope) {
   $scope.new_game = new_game;
-  $scope.angular_copy = function() {
+  $scope.angular_copy = function () {
     $scope.initial_count = initial_count;
     $scope.line_layer = line_layer;
     $scope.paper = paper;
     $scope.sprout_layer = sprout_layer;
-  }
+  };
 }
 
 function angular_copy() {
@@ -47,7 +52,7 @@ function line_links(line, sprout1, sprout2) {
   if (sprout1.data.links.length > 2) {
     sprout1.fillColor = dead_inside;
     sprout1.strokeColor = dead_outside;
-  };
+  }
   if (sprout2.data.links.length > 2) {
     sprout2.fillColor = dead_inside;
     sprout2.strokeColor = dead_outside;
@@ -59,7 +64,7 @@ function line_move(line, sprout1, delta1, sprout2, delta2) {
   var m1 = segments.length - 1;
   var delta = delta1;
   var epsilon = delta2.subtract(delta1).divide(m1);
-  segments.forEach(function(segment) {
+  segments.forEach(function (segment) {
     segment.point = segment.point.add(delta);
     delta = delta.add(epsilon);
   });
@@ -76,12 +81,12 @@ function line_move(line, sprout1, delta1, sprout2, delta2) {
 // ------
 
 function onMouseDown(event) {
-  if (selected_sprout != null) {
+  if (selected_sprout !== null) {
     dragged_sprout = selected_sprout;
     return;
   }
   starting_sprout = nearest_sprout(event.point);
-  if (starting_sprout == null) return;
+  if (starting_sprout === null) return;
   var point = starting_sprout.getNearestLocation(event.point).point;
   current_line = new paper.Path({
     segments: [point],
@@ -92,11 +97,11 @@ function onMouseDown(event) {
 }
 
 function onMouseDrag(event) {
-  if (dragged_sprout != null) {
+  if (dragged_sprout !== null) {
     sprout_move(dragged_sprout, event.point);
     return;
   }
-  if (current_line == null) return;
+  if (current_line === null) return;
   current_line.add(event.point);
   if (layer_touched(line_layer, current_line)) {
     current_line.remove();
@@ -105,11 +110,11 @@ function onMouseDrag(event) {
 }
 
 function onMouseUp(event) {
-  if (dragged_sprout != null) {
+  if (dragged_sprout !== null) {
     dragged_sprout = null;
     return;
   }
-  if (current_line == null) return;
+  if (current_line === null) return;
   if (current_line.segments.length < 3) {
     // Prevent sprouts overlapping.
     current_line.remove();
@@ -117,12 +122,12 @@ function onMouseUp(event) {
     return;
   }
   var ending_sprout = nearest_sprout(event.point);
-  if (ending_sprout == null) {
+  if (ending_sprout === null) {
     current_line.remove();
     current_line = null;
     return;
   }
-  if (ending_sprout == starting_sprout &&
+  if (ending_sprout === starting_sprout &&
       starting_sprout.data.links.length > 1) {
     // Prevent looping on a sprout with no room for two more links.
     current_line.remove();
@@ -160,9 +165,9 @@ var line_layer;
 
 function layer_touched(layer, path) {
   for (var counter = 0; counter < layer.children.length; counter++) {
-    var child = layer.children[counter]
-    if (path != child && path.getIntersections(child).length > 0) return true;
-  };
+    var child = layer.children[counter];
+    if (path !== child && path.getIntersections(child).length > 0) return true;
+  }
   return false;
 }
 
@@ -192,10 +197,10 @@ function nearest_sprout(point) {
   var ambiguous = true;
   var best_sprout = null;
   var best_distance;
-  sprout_layer.children.forEach(function(sprout) {
-    var distance = sprout.data.center.subtract(point).length
+  sprout_layer.children.forEach(function (sprout) {
+    var distance = sprout.data.center.subtract(point).length;
     if (distance < 100 && sprout.data.links.length < 3) {
-      if (best_sprout == null || 2 * distance < best_distance) {
+      if (best_sprout === null || 2 * distance < best_distance) {
         ambiguous = false;
         best_sprout = sprout;
         best_distance = distance;
@@ -211,17 +216,17 @@ function nearest_sprout(point) {
 function new_game(count) {
 
   // Decide how many sprouts.
-  if (count == null) {
+  if (count === null) {
     count = initial_count;
   } else {
     initial_count = count;
-  };
+  }
 
   // Erase any previous game.
-  line_layer.children.slice().forEach(function(line) {
+  line_layer.children.slice().forEach(function (line) {
     line.remove();
   });
-  sprout_layer.children.slice().forEach(function(sprout) {
+  sprout_layer.children.slice().forEach(function (sprout) {
     sprout.remove();
   });
 
@@ -229,12 +234,12 @@ function new_game(count) {
   var delta = 360 / count;
   var offset = new paper.Point();
   var grow = paper.view.size.divide(3);
-  offset.length = 1
+  offset.length = 1;
   offset.angle = Math.random() * delta;
   for (var counter = 0; counter < count; counter++) {
     sprout_new(paper.view.center.add(offset.multiply(grow)));
     offset.angle += delta;
-  };
+  }
 
   // Show them all.
   paper.view.draw();
@@ -243,11 +248,11 @@ function new_game(count) {
 
 function sprout_move(sprout, center) {
   var delta = center.subtract(sprout.data.center);
-  sprout.segments.forEach(function(segment) {
+  sprout.segments.forEach(function (segment) {
     segment.point = segment.point.add(delta);
-  })
+  });
   dragged_sprout.data.center = center;
-  sprout.data.links.forEach(function(link) {
+  sprout.data.links.forEach(function (link) {
     if (link.starting) {
       line_move(link.line,
                    sprout, delta, link.sprout, new paper.Point(0, 0));
@@ -259,7 +264,7 @@ function sprout_move(sprout, center) {
 }
 
 function sprout_new(center) {
-  sprout_layer.activate()
+  sprout_layer.activate();
   var sprout = new paper.Path.Circle({
     center: center,
     radius: radius,
@@ -268,9 +273,9 @@ function sprout_new(center) {
     strokeWidth: 4
   });
   sprout.data.center = center;
-  sprout.data.links = []
-  sprout.on('mouseenter', function() { sprout_select(sprout); });
-  sprout.on('mouseleave', function() { sprout_unselect(sprout); });
+  sprout.data.links = [];
+  sprout.on('mouseenter', function () { sprout_select(sprout); });
+  sprout.on('mouseleave', function () { sprout_unselect(sprout); });
   line_layer.activate();
   return sprout;
 }
@@ -281,11 +286,11 @@ function sprout_select(sprout) {
     sprout.fillColor = alive_over;
   } else {
     sprout.fillColor = dead_over;
-  };
+  }
   if (debug) {
-    sprout.data.links.forEach(function(link) {
+    sprout.data.links.forEach(function (link) {
       link.line.selected = true;
-    })
+    });
   }
 }
 
@@ -295,18 +300,18 @@ function sprout_unselect(sprout) {
     sprout.fillColor = alive_inside;
   } else {
     sprout.fillColor = dead_inside;
-  };
+  }
   if (debug) {
-    sprout.data.links.forEach(function(link) {
+    sprout.data.links.forEach(function (link) {
       link.line.selected = false;
-    })
+    });
   }
 }
 
 // Main program
 // ------------
 
-window.onload = function() {
+window.onload = function () {
 
   // Draw the initial display.
   paper.setup('sprouts');
@@ -321,4 +326,4 @@ window.onload = function() {
   tool.onMouseDown = onMouseDown;
   tool.onMouseDrag = onMouseDrag;
   tool.onMouseUp = onMouseUp;
-}
+};
